dbf55a19060135307f4531a66981a2fb491307b2
vipotin <vilma.pohjonen@gmail.com>
2022-01-29 18:20:56 +0200

    Update games route

--- pages/api/games.js
+++ pages/api/games.js
@@ -1,8 +1,10 @@
-const BASE_URL = "https://api.boardgameatlas.com/api/search"
-const CLIENT_ID=process.env.NEXT_PUBLIC_CLIENT_ID_TEST_CATEGORY
+import axios from "axios"
 
-export default async function getGamesByCategory(category) {
-  const response = await fetch(`${BASE_URL}?client_id=${CLIENT_ID}&categories=${category}`)
-  const data = await response.json()
-  return data.games
+const BASE_URL = "https://api.boardgameatlas.com/api"
+const CLIENT_ID=process.env.CLIENT_ID_TEST_CATEGORY
+
+export default async function getGames(req, res) {
+  const url = `${BASE_URL}/search?client_id=${CLIENT_ID}&categories=${req.query.category}`
+  const response = await axios.get(url)
+  return res.status(200).json({ data: response.data })
 }
