99ea89267160be499529b8fb138248f6dbe7a204
vipotin <vilma.pohjonen@gmail.com>
2022-01-29 18:21:36 +0200

    Use private client id

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.boardgameatlas.com/api"
-const CLIENT_ID=process.env.NEXT_PUBLIC_CLIENT_ID_TEST_CATEGORY
+const CLIENT_ID=process.env.CLIENT_ID_TEST_CATEGORY
 
 export default async function getCategories() {
   const response = await fetch(`${BASE_URL}/game/categories?client_id=${CLIENT_ID}`)
