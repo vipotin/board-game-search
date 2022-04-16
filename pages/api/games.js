d111cfdf483bb664eb717a4bca50ac94d2ae0cee
vipotin <vilma.pohjonen@gmail.com>
2022-04-16 19:35:53 +0300

    Add fields in a list

--- pages/api/games.js
+++ pages/api/games.js
@@ -2,9 +2,15 @@ import axios from "axios"
 
 const BASE_URL = "https://api.boardgameatlas.com/api"
 const CLIENT_ID=process.env.CLIENT_ID
+const fields = [
+  "name", 
+  "average_learning_complexity", 
+  "average_user_rating", 
+  "image_url"
+].join()
 
 export default async function getGames(req, res) {
-  const url = `${BASE_URL}/search?client_id=${CLIENT_ID}&categories=${req.query.category}`
+  const url = `${BASE_URL}/search?client_id=${CLIENT_ID}&categories=${req.query.category}&fields=${fields}`
   const response = await axios.get(url)
   return res.status(200).json({ data: response.data })
 }
