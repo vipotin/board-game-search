c52164c7740af8ded265efc15b8749c9e588c0ff
vipotin <vilma.pohjonen@gmail.com>
2022-04-17 21:07:25 +0300

    Add test for faulty category

--- __tests__/api.games.test.js
+++ __tests__/api.games.test.js
@@ -2,19 +2,26 @@ import { createMocks } from 'node-mocks-http';
 import getGames from '../pages/api/games';
 
 describe('/api/games', () => {
-  const category = 'v4SfYtS2Lr'
-  function mockRequestResponse(method = 'GET') {
+  function mockRequestResponse(category, method = 'GET') {
     const { req, res } = createMocks({ method });
     req.query = { category: `${category}` };
     return { req, res };
   }
-    test('should successfully returna an object with a name and image', async () => {
-        const { req, res } = mockRequestResponse();
-        await getGames(req, res);
-        expect(res.statusCode).toBe(200);
-        expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('name');
-        expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('average_user_rating');
-        expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('average_learning_complexity');
-        expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('image_url');
+    test('should successfully return a list of games with correct properties', async () => {
+      const category = 'v4SfYtS2Lr'
+      const { req, res } = mockRequestResponse(category);
+      await getGames(req, res);
+      expect(res.statusCode).toBe(200);
+      expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('name');
+      expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('average_user_rating');
+      expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('average_learning_complexity');
+      expect(JSON.parse(res._getData()).data.games[0]).toHaveProperty('image_url');
+    });
+
+    test('should return empty array with faulty category', async () => {
+      const category = 'not-a-category'
+      const { req, res } = mockRequestResponse(category);
+      await getGames(req, res);
+      expect(JSON.parse(res._getData()).data.count).toBe(0);
     });
 });
