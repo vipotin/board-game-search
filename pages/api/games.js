33ec9a49a645c88c538f103e5e0d5e466d2366b0
vipotin <vilma.pohjonen@gmail.com>
2022-05-07 18:18:52 +0300

    Delete search by field

--- pages/api/games.js
+++ pages/api/games.js
@@ -2,15 +2,9 @@ import axios from "axios"
 
 const BASE_URL = "https://api.boardgameatlas.com/api"
 const CLIENT_ID=process.env.CLIENT_ID
-const fields = [
-  "name", 
-  "average_learning_complexity", 
-  "average_user_rating", 
-  "image_url"
-].join()
 
 export default async function getGames(req, res) {
-  const url = `${BASE_URL}/search?client_id=${CLIENT_ID}&categories=${req.query.category}&fields=${fields}`
+  const url = `${BASE_URL}/search?client_id=${CLIENT_ID}&categories=${req.query.category}`
   await axios
     .get(url)
     .then(({ data }) => {
